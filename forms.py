1e33740a6f973f30b19e4f8fa1b369a195c5dd4c
Dod0 <117435581+Nanuk007@users.noreply.github.com>
2025-02-17 22:09:55 +0100

    structure add

--- forms.py
+++ forms.py
@@ -1,1 +1,20 @@
-lc
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, TextAreaField, SubmitField, PasswordField
+# from wtforms.validators import DataRequired, Email, EqualTo
+
+# class PridajClanokFormular(FlaskForm):
+#     titulka = StringField('Názov článku', validators=[DataRequired()])
+#     obsah = TextAreaField('Obsah článku', validators=[DataRequired()])
+#     odoslat = SubmitField('Pridaj článok')
+
+# class RegistrationForm(FlaskForm):
+#     username = StringField('Používateľské meno', validators=[DataRequired()])
+#     email = StringField('Email', validators=[DataRequired(), Email()])
+#     password = PasswordField('Heslo', validators=[DataRequired()])
+#     password_confirm = PasswordField('Potvrďte heslo', validators=[DataRequired(), EqualTo('password')])
+#     submit = SubmitField('Registrovať')
+
+# class LoginForm(FlaskForm):
+#     email = StringField('Email', validators=[DataRequired(), Email()])
+#     password = PasswordField('Heslo', validators=[DataRequired()])
+#     submit = SubmitField('Prihlásiť sa')
