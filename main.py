0fd08d46803a2d9aab246541630525771cc30371
Mardonnass <stevhuda@gmail.com>
2025-02-28 07:30:46 +0100

    aspon to ide spustis

--- main.py
+++ main.py
@@ -1,113 +1,80 @@
 from flask import Flask
 from flask import render_template, url_for, redirect, request, flash
-# from flask_login import LoginManager
-# from forms import PridajClanokFormular
-# from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
-# from forms import RegistrationForm, LoginForm
-from models import db, Admin, Client
+from flask_login import LoginManager
+from forms import PridajClanokFormular
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
+from forms import RegistrationForm, LoginForm
+from models import Clanok, db, User
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hotelcek.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clanky.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-app.secret_key = 'macak'
+app.secret_key = 'tvoj_krastny_sigma_klucik'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader 
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/admin/login')
-def admin(id):
-    return render_template('admin-log.html', id=id)
+@app.route('/roomselect')
+def roomselect():
+    return render_template('roomselect.html')
 
-# @app.route('/clanky')
-# def clanky():
-#     cl = Clanok.query.all()
-#     return render_template('clanky.html', clanky=cl)
+@app.route('/onas')
+def onas():
+    return render_template('onas.html')
 
-# @app.route('/onas')
-# def onas():
-#     return render_template('onas.html')
+@app.route('/adminLog')
+def admin():
+    return render_template('admin-log.html')
 
-# @app.route('/profil')
-# def profil():
-#     return render_template('profil.html')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            flash('Email už existuje', 'error')
+            return redirect(url_for('register'))
 
-# @app.route('/pridaj_clanok', methods=['POST', 'GET'])
-# def pridaj_clanok():
-#     f = PridajClanokFormular()
-#     print('idzem')
-#     print(f.validate_on_submit())
-#     if f.validate_on_submit():
-#         print('aj tu idzem')
-#         nc = Clanok(nazov=f.titulka.data, obsah=f.obsah.data)
-#         db.session.add(nc)
-#         db.session.commit()
-#         return redirect(url_for('clanky'))
-#     return render_template('pridaj_clanok.html', form = f)
+        new_user = User(username=form.username.data, email=form.email.data)
+        new_user.set_password(form.password.data)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Registrovaný úspešne!', 'success')
+        return redirect(url_for('login'))
 
-# @app.template_filter('shorten')
-# def shorten_text(text, length=100):
-#     if len(text) > length:
-#         return text[:length] + '...'  # Skrátenie a pridanie trojbodky
-#     return text
+    return render_template('register.html', form=form)
 
-# @app.route('/clanok/<int:id>')
-# def detail_clanku(id):
-#     # Načítanie článku podľa ID
-#     clanok = Clanok.query.get_or_404(id)
-#     return render_template('clanok.html', clanok=clanok)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            flash('Prihlásenie úspešné', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Nesprávny email alebo heslo', 'error')
 
-# @app.route('/upravit_clanok/<int:clanok_id>', methods=['GET', 'POST'])
-# def upravit_clanok(clanok_id):
-#     clanok = Clanok.query.get_or_404(clanok_id)  # Načítanie článku podľa ID
-#     form = PridajClanokFormular()  # Tento formulár môže byť rovnaký ako na pridanie článku
+    return render_template('login.html', form=form)
 
-#     # Predvyplnenie formuláru s existujúcimi hodnotami článku
-#     if request.method == 'GET':
-#         form.titulka.data = clanok.nazov
-#         form.obsah.data = clanok.obsah
-
-#     if form.validate_on_submit():  # Ak formulár bol odoslaný
-#         clanok.nazov = form.titulka.data  # Aktualizácia názvu
-#         clanok.obsah = form.obsah.data    # Aktualizácia obsahu
-#         db.session.commit()  # Uloženie zmien do databázy
-#         return redirect(url_for('clanky'))  # Presmerovanie na zoznam článkov
-
-#     return render_template('uprav_clanok.html', form=form, clanok=clanok)
-
-# @app.route('/register', methods=['GET', 'POST'])
-# def register():
-#     form = RegistrationForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         if user:
-#             flash('Email už existuje')
-#             return redirect(url_for('register'))
-
-#         new_user = User(username=form.username.data, email=form.email.data)
-#         new_user.set_password(form.password.data)
-#         db.session.add(new_user)
-#         db.session.commit()
-#         flash('Registrovaný úspešne!')
-#         return redirect(url_for('login'))
-
-#     return render_template('register.html', form=form)
-
-# @app.route('/login', methods=['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(email=form.email.data).first()
-#         if user and user.check_password(form.password.data):
-#             flash('Prihlásenie úspešné')
-#             return redirect(url_for('home'))
-#         else:
-#             flash('Nesprávny email alebo heslo')
-
-#     return render_template('login.html', form=form)
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('Úspešne ste sa odhlásili!', 'success')
+    return redirect(url_for('login'))
 
 with app.app_context():
     db.create_all()
--- models.py
+++ models.py
@@ -4,25 +4,23 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-#admin user
-class Admin(UserMixin, db.Model):
+class Clanok(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    adminID = db.Column(db.String(20), unique=True, nullable=False)
+    nazov = db.Column(db.String(100), nullable=False)
+    obsah = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f'Článok: {self.nazov}'
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-class Client(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    phone = db.Column(db.String(20), unique=True, nullable=False)
-    dateOfReservation = db.Column(db.String(20), unique=True, nullable=False)
-    payMetod = db.Column(db.String(20), unique=True, nullable=False)
-    headCount = db.Column(db.Integer, unique=True, nullable=False)
-
-
-    
