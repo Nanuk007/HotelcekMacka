55d1862f796eba86c6c8899d43952b80fdc21164
nanuk007 <karaffa.filip@gmail.com>
2025-03-16 17:07:02 +0100

    order room edit

--- static/js/animations.js
+++ static/js/animations.js
@@ -1,19 +1,28 @@
 let index = 0;
     const images = document.querySelectorAll(".slider-container img");
     const totalImages = images.length;
-    
     function showSlide() {
         document.querySelector(".slider-container").style.transform = `translateX(${-index * 100}%)`;
     }
-    
     function nextSlide() {
         index = (index + 1) % totalImages;
         showSlide();
     }
-    
     function prevSlide() {
         index = (index - 1 + totalImages) % totalImages;
         showSlide();
     }
-    
     setInterval(nextSlide, 5000);
+
+function updateRoomImage() {
+    let roomSelect = document.getElementById("room").value;
+    let roomImage = document.getElementById("room-image");
+
+    let images = {
+        "room1": "images/room1.jpg",
+        "room2": "images/room2.jpg",
+        "room3": "images/room3.jpg"
+    };
+
+    roomImage.src = images[roomSelect] || "images/default-room.jpg";
+}
